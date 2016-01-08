ce497e4617790b4a3ee35006a815e7641c6d19f4
digitalmuddler <digitalmuddler@users.noreply.github.com>
2016-01-07 22:54:47 -0500

    Create ConvertTemperature.cpp

---
+++ CPP/ConvertTemperature.cpp
@@ -0,0 +1,8 @@
+// simple temperature conversion functions
+
+#include <iostream.h>
+#include <iomanip.h>
+
+double FahrenheitToCelsius ( double fah ) { return 5.0 / 9.0 * ( fah - 32.0 ); }
+
+double CelsiusToFahrenheit ( double cel ) {return 9.0 / 5.0 * ( cel + 32 ); }
