5962b6e69b758eb0adffff1fc40834d7f5019ab7
digitalmuddler <digitalmuddler@users.noreply.github.com>
2016-01-08 09:54:51 -0500

    Update and rename ConvertTemperature.cpp to convert.h

--- CPP/convert.h
+++ CPP/convert.h
@@ -1,7 +1,7 @@
-// simple temperature conversion functions
+#ifndef convert
+#define convert
 
-#include <iostream.h>
-#include <iomanip.h>
+// Temperature Conversion
 
 // from Fahrenheit
 double FahrenheitToCelsius ( double x ) { return 5.0 / 9.0 * ( x - 32.0 ); }
@@ -15,5 +15,5 @@ double CelsiusToKelvin ( double x ) { return x + 273.15; }
 double KelvinToFahrenheit (double x ) { return x - 459.67; }  // don't believe this is correct ?
 double KelvinToCelsius ( double x ) { return x - 273.15; }
 
-
+// Measurement conversion
 
